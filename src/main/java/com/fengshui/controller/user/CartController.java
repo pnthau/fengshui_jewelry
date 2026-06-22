57d9f98e67397448e6bd4a7fa50fb98c84c74940
pnthau <pnguyentrunghau.24@gmail.com>
2026-06-22 17:49:37 +0700

    update cart feature : thây đổi dừng dẫn của controller thành /WEB-INF/views/user/product_list.jsp

--- src/main/java/com/fengshui/controller/user/CartController.java
+++ src/main/java/com/fengshui/controller/user/CartController.java
@@ -23,7 +23,7 @@ public class CartController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        req.getRequestDispatcher("product_list.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/user/product_list.jsp").forward(req, resp);
     }
 
     @Override
